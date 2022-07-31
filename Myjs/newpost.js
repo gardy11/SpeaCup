929c6353d3fdb3c95687989993a87d1811edfefe
gardy11 <106308847+gardy11@users.noreply.github.com>
2022-07-31 23:46:21 +0800

    Update newpost.js

--- Myjs/newpost.js
+++ Myjs/newpost.js
@@ -63,24 +63,14 @@ const form = document.querySelector( ".posting-area" ),
           input.setAttribute('accept', 'image/*');
           input.setAttribute('max-width', '10px');
       
-          /*
-            Note: In modern browsers input[type="file"] is functional without
-            even adding it to the DOM, but that might not be the case in some older
-            or quirky browsers like IE, so you might want to add it to the DOM
-            just in case, and visually hide it. And do not forget do remove it
-            once you do not need it anymore.
-          */
+         
       
           input.onchange = function () {
             var file = this.files[0];
       
             var reader = new FileReader();
             reader.onload = function () {
-              /*
-                Note: Now we need to register the blob in TinyMCEs image blob
-                registry. In the next release this part hopefully won't be
-                necessary, as we are looking to handle it internally.
-              */
+            
     
               var id = 'blobid' + (new Date()).getTime();
               var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
