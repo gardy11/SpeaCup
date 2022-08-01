591c13d8f2296ff77f8a03c5831c8386ab481bac
gardy11 <106308847+gardy11@users.noreply.github.com>
2022-08-01 19:27:56 +0800

    Update edit_post.php

--- edit_post.php
+++ edit_post.php
@@ -223,7 +223,7 @@ while ($row = mysqli_fetch_assoc($query)) {
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
       <script src="./JS/like_dislike.js"></script>
       <script src="./JS/collection.js"></script>
-      <script src="./Myjs/post.js"></script>
+      <script src="./JS/post.js"></script>
 
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
