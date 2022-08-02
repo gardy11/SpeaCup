0574052908c92e3e417516fb36dd04a0743eede4
inkei320 <107203204+inkei320@users.noreply.github.com>
2022-08-02 09:32:50 +0800

    0802

--- member.php
+++ member.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION['unique_id'])) { //未登入時顯示請登入
 }
 ?>
 
-if 
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -78,7 +78,7 @@ if
                               <button class="btn btn-outline-danger my-2 my-sm-0 " type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
                         </form>
                         <li class="nav-item pl-5 mr-5">
-             
+
                         <li><a class="fa-solid fa-user mt-2" onclick="doAnimateShow();">&ensp;&ensp;</a></li>
                         </li>
             </div>
@@ -104,17 +104,15 @@ if
                         <i class="fas fa-align-left"></i>
                   </button>
                   <ul class="list-unstyled p-1 ">
-                  <li style="background-color:wheat;">
+                        <li style="background-color:wheat;">
                               <a href="member.php" calss="m-2">基本資料<i class="fas mt-1 fa-solid fa-user-check"></i></i>
                               </a>
                         </li>
                         <li>
-                              <a href="collections.php" calss="m-2">收藏文章<i
-                                          class="fas mt-1 fa-solid fa-file-circle-plus"></i></i> </a>
+                              <a href="collections.php" calss="m-2">收藏文章<i class="fas mt-1 fa-solid fa-file-circle-plus"></i></i> </a>
                         </li>
                         <li>
-                              <a href="posts.php" calss="p-2">發表過文章<i
-                                          class="fas mt-1 fa-regular fa-pen-to-square"></i></i> </a>
+                              <a href="posts.php" calss="p-2">發表過文章<i class="fas mt-1 fa-regular fa-pen-to-square"></i></i> </a>
                         </li>
                         <li>
                               <a href="friends.php" calss="m-2">好友列表<i class="fas mt-1  fa-solid fa-users"></i> </a>
@@ -142,8 +140,7 @@ if
                         </div>
                         <div id="m-nickname" class="m-3 field input">
                               <h4 class="ml-5 text-muted">暱稱</h4>
-                              <input type="text" name="fname" value="<?php echo $row['nickname']; ?>" id="m-nickname-n"
-                                    class="ml-5" required />
+                              <input type="text" name="fname" value="<?php echo $row['nickname']; ?>" id="m-nickname-n" class="ml-5" required />
                               <!-- <p id="m-nickname-n" class="ml-5"><?php echo $row['nickname']; ?></p> -->
                               <hr class="hr">
                         </div>
@@ -151,8 +148,7 @@ if
                         <div id="m-password" class="m-3">
                               <h4 class="ml-5 text-muted">密碼</h4>
                               <div class="row ">
-                                    <button class="btn btn-outline-warning ml-5 col-2"
-                                          onclick="location.href='update_pw.php'">更改密碼</button>
+                                    <button class="btn btn-outline-warning ml-5 col-2" onclick="location.href='update_pw.php'">更改密碼</button>
 
                               </div>
                               <hr class="hr">
@@ -164,15 +160,12 @@ if
 
                                     <input type="hidden" name="old_img" value="/<?php echo $row['img']; ?>" />
 
-                                    <img src="php/images/<?php echo $row['img']; ?>" width="200px" height="200px"
-                                          class="ml-5 col-3 rounded" alt="大頭貼">
+                                    <img src="php/images/<?php echo $row['img']; ?>" width="200px" height="200px" class="ml-5 col-3 rounded" alt="大頭貼">
 
                                     <div class="col" style="padding-left:10%">
                                           <div class="input-group mb-1 pb-5 col-7 field image" width="200px">
-                                                <img id="preview_img" src="php/images/review.png" width="200px"
-                                                      height="200px" style="padding-bottom:2%" />
-                                                <input type="file" name="image" accept="image/*,.jpeg, .png, .jpg"
-                                                      class="form-control " id="inputGroupFile02" width="200px">
+                                                <img id="preview_img" src="php/images/review.png" width="200px" height="200px" style="padding-bottom:2%" />
+                                                <input type="file" name="image" accept="image/*,.jpeg, .png, .jpg" class="form-control " id="inputGroupFile02" width="200px">
                                           </div>
                                     </div>
 
@@ -184,19 +177,18 @@ if
                         <div id="m-bd" class="m-3">
                               <h4 class="ml-5 text-muted">生日</h4>
                               <?php if ($row['birth'] != "0000-00-00") { ?>
-                              <!-- google登入者須填寫生日，一般登入回傳預設值 -->
-                              <p id="m-nickname-n" class="ml-5"><?php echo $row['birth']; ?></p>
-                              <!-- 隱藏區塊 回傳預設值到後端以進行更新作業 -->
-                              <div id="r-bd-r" class="ml-5 field" style="display:none">
-                                    <input type="date" name="birth" id="r-bd" value="<?php echo $row['birth'] ?>"
-                                          required />
-                              </div>
+                                    <!-- google登入者須填寫生日，一般登入回傳預設值 -->
+                                    <p id="m-nickname-n" class="ml-5"><?php echo $row['birth']; ?></p>
+                                    <!-- 隱藏區塊 回傳預設值到後端以進行更新作業 -->
+                                    <div id="r-bd-r" class="ml-5 field" style="display:none">
+                                          <input type="date" name="birth" id="r-bd" value="<?php echo $row['birth'] ?>" required />
+                                    </div>
                               <?php } else { ?>
-                              <!-- 限制日期只可選取不可自行輸入 -->
-                              <div id="r-bd-r" class="ml-5 field">
-                                    <input type="date" name="birth" id="r-bd" onkeydown="return false" required />
-                                    <label style="color: red;padding-left:2.5%;">*生日待填寫(填好不可修改)</label>
-                              </div>
+                                    <!-- 限制日期只可選取不可自行輸入 -->
+                                    <div id="r-bd-r" class="ml-5 field">
+                                          <input type="date" name="birth" id="r-bd" onkeydown="return false" required />
+                                          <label style="color: red;padding-left:2.5%;">*生日待填寫(填好不可修改)</label>
+                                    </div>
                               <?php  } ?>
 
                               <hr class="hr">
@@ -205,26 +197,21 @@ if
                         <div id="m-bd" class="m-3 mt-2">
                               <h4 class="ml-5 text-muted">性別：</h4>
                               <?php if ($row['gender'] != "") { ?>
-                              <!-- google登入者須填寫性別，一般登入回傳預設值 -->
-                              <p id="m-nickname-n" class="ml-5"><?php echo $row['gender']; ?></p>
-                              <!-- 隱藏區塊 回傳預設值到後端以進行更新作業 -->
-                              <div class="gender m-3 mt-2" style="display:none">
-                                    <input id="g1" type="radio" name="gender" value="男" class="ml-5" required /><label
-                                          for="g1">男</label>
-                                    <input id="g2" type="radio" name="gender" value="女" class="ml-5" required /><label
-                                          for="g2">女</label>
-                                    <!-- 未勾選按鈕的情況下 回傳預設值 -->
-                                    <input type="hidden" name="gender" value="<?php echo $row['gender']; ?>"
-                                          class="ml-5" required />
-                              </div>
+                                    <!-- google登入者須填寫性別，一般登入回傳預設值 -->
+                                    <p id="m-nickname-n" class="ml-5"><?php echo $row['gender']; ?></p>
+                                    <!-- 隱藏區塊 回傳預設值到後端以進行更新作業 -->
+                                    <div class="gender m-3 mt-2" style="display:none">
+                                          <input id="g1" type="radio" name="gender" value="男" class="ml-5" required /><label for="g1">男</label>
+                                          <input id="g2" type="radio" name="gender" value="女" class="ml-5" required /><label for="g2">女</label>
+                                          <!-- 未勾選按鈕的情況下 回傳預設值 -->
+                                          <input type="hidden" name="gender" value="<?php echo $row['gender']; ?>" class="ml-5" required />
+                                    </div>
                               <?php } else { ?>
-                              <div class="gender m-3 mt-2">
-                                    <input id="g1" type="radio" name="gender" value="男" class="ml-5" required /><label
-                                          for="g1">男</label>
-                                    <input id="g2" type="radio" name="gender" value="女" class="ml-5" required /><label
-                                          for="g2">女</label>
-                                    <label style="color: red; padding-left:3%;">*性別待填寫(填好不可修改)</label>
-                              </div>
+                                    <div class="gender m-3 mt-2">
+                                          <input id="g1" type="radio" name="gender" value="男" class="ml-5" required /><label for="g1">男</label>
+                                          <input id="g2" type="radio" name="gender" value="女" class="ml-5" required /><label for="g2">女</label>
+                                          <label style="color: red; padding-left:3%;">*性別待填寫(填好不可修改)</label>
+                                    </div>
                               <?php  } ?>
 
                               <hr class="hr">
@@ -234,14 +221,12 @@ if
                         <div id="m-email" class="m-3 field input">
                               <h4 class="ml-5 text-muted">綁定信箱</h4>
                               <?php if ($row['mid'] != 0) { ?>
-                              <!-- 一般登入信箱可修改，google登入信箱不可修改-->
-                              <!-- 傳回預設值以讓後端更新-->
-                              <input type="email" name="email" value="<?php echo $row['email']; ?>" id="m-email-e"
-                                    class="ml-5 " readonly required />
-                              <label style="color: red; padding-left:3%;">*使用google登入的帳戶信箱不可修改</label>
+                                    <!-- 一般登入信箱可修改，google登入信箱不可修改-->
+                                    <!-- 傳回預設值以讓後端更新-->
+                                    <input type="email" name="email" value="<?php echo $row['email']; ?>" id="m-email-e" class="ml-5 " readonly required />
+                                    <label style="color: red; padding-left:3%;">*使用google登入的帳戶信箱不可修改</label>
                               <?php } else { ?>
-                              <input type="email" name="email" value="<?php echo $row['email']; ?>" id="m-email-e"
-                                    class="ml-5 " required />
+                                    <input type="email" name="email" value="<?php echo $row['email']; ?>" id="m-email-e" class="ml-5 " required />
                               <?php  } ?>
 
                         </div>
@@ -255,8 +240,7 @@ if
 
                         <div class="error-txt"></div>
                         <div id="m-c" class="m-3 field button">
-                              <input type="submit" name="submit2" class="btn btn-outline-warning  m-3 ml-5 mb-5"
-                                    value="儲存修改">
+                              <input type="submit" name="submit2" class="btn btn-outline-warning  m-3 ml-5 mb-5" value="儲存修改">
                         </div>
 
 
@@ -281,59 +265,56 @@ if
 
 
 <script>
-// 回到頂端樣式
-$(function() {
-      var $win = $(window);
-      var $backToTop = $('.js-back-to-top');
+      // 回到頂端樣式
+      $(function() {
+            var $win = $(window);
+            var $backToTop = $('.js-back-to-top');
 
-      $win.scroll(function() {
-            if ($win.scrollTop() > 300) {
-                  $backToTop.show();
-            } else {
-                  $backToTop.hide();
-            }
+            $win.scroll(function() {
+                  if ($win.scrollTop() > 300) {
+                        $backToTop.show();
+                  } else {
+                        $backToTop.hide();
+                  }
+            });
+
+
+            $backToTop.click(function() {
+                  $('html, body').animate({
+                        scrollTop: 0
+                  }, 200);
+            });
       });
-
-
-      $backToTop.click(function() {
-            $('html, body').animate({
-                  scrollTop: 0
-            }, 200);
-      });
-});
 </script>
 
 <script>
-//選擇大頭貼預覽
+      //選擇大頭貼預覽
 
-function readURL(input) {
-      if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                  if (input.files[0]) {
+      function readURL(input) {
+            if (input.files && input.files[0]) {
+                  var reader = new FileReader();
+                  reader.onload = function(e) {
+                        if (input.files[0]) {
 
+                        }
+                        $("#preview_img").attr('src', e.target.result);
                   }
-                  $("#preview_img").attr('src', e.target.result);
+                  reader.readAsDataURL(input.files[0]);
             }
-            reader.readAsDataURL(input.files[0]);
       }
-}
 
-$("#inputGroupFile02").change(function() {
+      $("#inputGroupFile02").change(function() {
 
-      readURL(this);
+            readURL(this);
 
-});
-//會員中心
-function doAnimateShow() {
+      });
+      //會員中心
+      function doAnimateShow() {
             document.getElementById("box").style.top = "90px";
             event.cancelBubble = true;
       }
-</script>
 
-
-<script>
-      function openArticle(evt, articleName) {
+       function openArticle(evt, articleName) {
             var i, x, tablinks;
             x = document.getElementsByClassName("article");
             for (i = 0; i < x.length; i++) {
@@ -396,4 +377,9 @@ function doAnimateShow() {
       });
 </script>
 
-</html>
+
+<script>
+     
+</script>
+
+</html> 
